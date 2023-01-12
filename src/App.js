a38a11841aca255af704c60cd852615a4ca88889
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-12 15:36:42 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import MealsSummary from "./components/Meals/MealsSummary";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <MealsSummary />
+    </div>
+  );
+}
+
+export default App;
