e7cb0c7b38fee36e19b2f7687e0e7143a28042d5
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-17 20:55:23 +0000

    added loader and loading text + animation

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,53 +6,85 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchMealsHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://movie-database-c5c02-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
-      );
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        // Fetch existing meals from firebase
+        const response = await fetch(
+          "https://food-order-app-4f64b-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+        );
 
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const data = await response.json();
+
+        const loadedMeals = [];
+
+        // Extract meal details from the firebase data
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        // delay test = Start
+        function timeout(delay) {
+          return new Promise((res) => setTimeout(res, delay));
+        }
+        await timeout(5000); //for 1 sec delay
+        // delay test = End
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
       }
-
-      const data = await response.json();
-      // console.log(`content of the "data" constant: ${data}`);
-
-      const loadedMeals = [];
-
-      for (const key in data) {
-        loadedMeals.push({
-          description: data[key].description,
-          id: key,
-          name: data[key].name,
-          price: data[key].price,
-        });
-      }
-      setMeals(loadedMeals);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
+    };
+    fetchMeals();
   }, []);
 
-  useEffect(() => {
-    fetchMealsHandler();
-  }, [fetchMealsHandler]);
+  // Send order to firebase
+  // async function sendOrderHandler(order) {
+  //   const response = await fetch(
+  //     "https://food-order-app-4f64b-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(order),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
 
+  // Error handling
   let content = <p>Found no meals</p>;
   let mealsList = [];
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className={classes.loading}>Loading...</p>;
+        <p className={classes.loader}>...</p>;
+      </section>
+    );
+  }
 
   if (meals.length > 0) {
     mealsList = meals.map((meal) => (
       <MealItem
-        id={meal.id}
         key={meal.id}
+        id={meal.id}
         name={meal.name}
         description={meal.description}
         price={meal.price}
@@ -62,10 +94,6 @@ const AvailableMeals = () => {
 
   if (error) {
     content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
   }
 
   // console.log(mealsList);
